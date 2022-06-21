556f4c0b48cae03a06a1cab0298513d38699f8c6
nazap31 <you@example.com>
2022-06-20 23:49:09 -0600

    pares y impares

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine();
+// int number = input.parseInt();
+int number = Int32.Parse(input);
+
+int module = number % 2;
+
+if (module == 0)
+{
+Console.WriteLine("Even");
+} else {
+ Console.WriteLine("odd");
+}   
